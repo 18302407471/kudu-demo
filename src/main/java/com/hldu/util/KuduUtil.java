7ec77082cb2c2859b133f0c7133b00e93da6b79e
18302407471 <49739123+18302407471@users.noreply.github.com>
2019-05-30 15:42:10 +0800

    Update KuduUtil.java

--- src/main/java/com/hldu/util/KuduUtil.java
+++ src/main/java/com/hldu/util/KuduUtil.java
@@ -4,7 +4,7 @@ import org.apache.kudu.client.KuduClient;
 
 public class KuduUtil {
 
-    private static final String MASTERADDRESS = "10.11.2.1:7051";
+    private static final String MASTERADDRESS = "地址:7051";
 
     public static KuduClient getConf(){
         KuduClient conf = new KuduClient.KuduClientBuilder(MASTERADDRESS).defaultSocketReadTimeoutMs(6000).build();
